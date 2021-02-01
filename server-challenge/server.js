54cadfc7880cee66a8a83ea487923aeb6dcacc9a
Roxana C <roxana.c2007@gmail.com>
2021-02-01 23:10:52 +0000

    outline

---
+++ server-challenge/server.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const path = require("path")
+const cors = require("cors")
+
+[POST] /v3/discover
+[POST] /v3/enrich/assets
+[GET] /v3/enrich/jobs
+[GET] /v3/search/*
